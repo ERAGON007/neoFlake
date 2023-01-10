b79174d4af76fbcbafb0a99b9fe7055ff36e48e4
ERAGON007 <turk6486@gmail.com>
2023-01-10 21:30:54 +0330

    Load home-manager configs from home/

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
             home-manager.backupFileExtension = "backup";
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.eragon = import ./home.nix;
+            home-manager.users.eragon = import ./home;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
